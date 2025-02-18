0c694036678231ec8677f2e414440d252430e3a1
Arshad Khan <108711604+arshadkh507@users.noreply.github.com>
2025-02-18 11:29:08 +0500

    Tailwind css setup done with typescript

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+// import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <h1 className="text-2xl text-blue-400">
+        Welcome to nextjs v15 with reactjs v19 plus tailwind v4
+      </h1>
+    </div>
+  );
+}
